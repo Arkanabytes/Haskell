823ed478f4e9e465dee109018590d82c86a29fd2
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-24 12:56:13 +0200

    .

---
+++ Problem_3/Solution_1.3.hs
@@ -0,0 +1,6 @@
+--1.3 Volumen de la esfera
+--Definir la funcion volumenEsfera tal que (volumenEsfera r) es el volumen de la esfera de radio r, por ejemplo
+--indicacion constante utilizacion de pi.
+
+--solucion:
+volumenEsfera r = (4/3)*pi*r^3
