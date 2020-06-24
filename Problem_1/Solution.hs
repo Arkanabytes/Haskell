9cdd50d57ac8045d63da9bb15963656f8dad4483
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-24 12:51:01 +0200

    .

--- Problem_1/Solution.hs
+++ Problem_1/Solution.hs
@@ -1,1 +1,8 @@
+--1.1 Media de 3 numeros 
+--Definir la funcion media de 3 numeros tal que (media3 x y z ) es la aritmetica de los numeros x, y y z. Por ejemplo:
+-- media3    1  3  8 == 4.0
+-- media3 (-1)  0  7 == 2.0
+-- media3 (-3)  0  3 == 0.0
+
+-- Solucion
 media3 x y z = (x+y+z)/3
