eabcaae15d08352a5ac19b13adc73b6d13875d48
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-26 19:38:02 +0200

    .

---
+++ Problem_16.2/1.16.2_Igualdas_diferencia_tres_elementos_solution.hs
@@ -0,0 +1,8 @@
+-1.16.2 Igualdas y diferencia de tres elementos
+--Definir la funcion tresDiferentes tal que (tresDifenrentes x y z) se verifica
+--si los elementos x, y, z son distintos,Por ejemplo.
+------tresDiferentes 3 5 2 == True
+------tresDiferentes 3 5 3 == False
+
+--Solucion 
+tresDiferentes x y z = x /= y && x /= z && y /= z
