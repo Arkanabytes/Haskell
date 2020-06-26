b85a9aff45bb44edba84ce136d3976781aef0e18
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-06-26 19:38:43 +0200

    .

--- Problem_16.2/1.16.2_Igualdas_diferencia_tres_elementos_solution.hs
+++ Problem_16.2/1.16.2_Igualdas_diferencia_tres_elementos_solution.hs
@@ -1,4 +1,4 @@
--1.16.2 Igualdas y diferencia de tres elementos
+--1.16.2 Igualdas y diferencia de tres elementos
 --Definir la funcion tresDiferentes tal que (tresDifenrentes x y z) se verifica
 --si los elementos x, y, z son distintos,Por ejemplo.
 ------tresDiferentes 3 5 2 == True
