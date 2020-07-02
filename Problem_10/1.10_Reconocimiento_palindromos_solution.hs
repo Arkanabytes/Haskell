4d015902c7eef47d0087ac30eb6ff833ecb4f508
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-02 23:47:31 +0200

    .

--- Problem_10/1.10_Reconocimiento_palindromos_solution.hs
+++ Problem_10/1.10_Reconocimiento_palindromos_solution.hs
@@ -1,3 +1,4 @@
+--#Arkana#
 --1.10 Reconocimiento de palindromos
 ---Definir la funcion palindromo tal que (palindromo xs) se verifica si xs es una palindromo ; es decir es lo mismo que leer xs de izquierda a derecha que de derecha a izquierda.
 ---Por ejemplo,
