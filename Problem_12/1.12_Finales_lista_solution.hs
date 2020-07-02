f3284ce247ef361769d473d4fac487daaa438ffa
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-02 23:48:50 +0200

    .

--- Problem_12/1.12_Finales_lista_solution.hs
+++ Problem_12/1.12_Finales_lista_solution.hs
@@ -1,3 +1,4 @@
+--#Arkana#
 --1.12 Finales de una lista 
 ---Definir la funcion finales  tal que (finales n xs) es la lista formada por los n finales elementos de xs. Por ejemplo.
 -----finales 3 [2,5,4,7,9,6] == [7,9,6]
