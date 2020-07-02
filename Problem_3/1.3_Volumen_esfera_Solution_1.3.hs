1019af1cc00997ca0002d036e537ef2b45c370b1
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-02 23:43:47 +0200

    .

--- Problem_3/1.3_Volumen_esfera_Solution_1.3.hs
+++ Problem_3/1.3_Volumen_esfera_Solution_1.3.hs
@@ -1,3 +1,5 @@
+--#Arkana#
+
 --1.3 Volumen de la esfera
 --Definir la funcion volumenEsfera tal que (volumenEsfera r) es el volumen de la esfera de radio r, por ejemplo
 --indicacion constante utilizacion de pi.
