2f87bb1456e24ec0d9f1959523268971ea74044d
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-11 02:35:10 +0200

    .

---
+++ Problem_1.19/1.19_Division_segura.hs
@@ -0,0 +1,9 @@
+--1.19 Division Segura
+---Ejercicio Definir la funcion divisionSegura tal que (divisionSegura x y ) es
+---x/y si y no es cero y 999 en caso contrario.Por ejemplo,
+----DivisionSeguro 7  2  == 3.5
+----DivisionSeguro 7  0  == 9999.0
+
+--Solucion
+divisionSegura _ 0 = 9999
+divisionSegura x y = x/y
