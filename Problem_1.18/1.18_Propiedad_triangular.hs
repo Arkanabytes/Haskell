2f6d70fba84ab3af82add12f20aa596a5b03d1e1
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-02 23:03:20 +0200

    .

--- Problem_1.18/1.18_Propiedad_triangular.hs
+++ Problem_1.18/1.18_Propiedad_triangular.hs
@@ -2,10 +2,8 @@
 
 --1.18 Propiedad trianqular
 --Las longitudes de los lados de un triangulo no pueden ser cualesquiera.Para que pueda construirse el triangulo, 
---tiene que cumplirse la propiedad triangular; es decir, longuitud de cada lado tiene que ser un menor que la suma
---de los otros dos lados.
----Definir la funcion triangular tal que(triangular a b c) se verifica si a, b y c cumplen la propiedad triangular
----Por, ejemplo.
+--tiene que cumplirse la propiedad triangular; es decir, longuitud de cada lado tiene que ser un menor que la suma de los otros dos lados.
+---Definir la funcion triangular tal que(triangular a b c) se verifica si a, b y c cumplen la propiedad triangular Por, ejemplo.
 
 -------triangular  3  4  5 == True 
 -------triangular 30  4  5 == True 
