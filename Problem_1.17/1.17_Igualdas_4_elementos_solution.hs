329cc1564ef1d5675feaf22efe9afb19bea21ebc
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-02 23:02:11 +0200

    .

--- Problem_1.17/1.17_Igualdas_4_elementos_solution.hs
+++ Problem_1.17/1.17_Igualdas_4_elementos_solution.hs
@@ -1,8 +1,7 @@
 --Arkana
 
 ---1.17 Igualdas de 4 elementos
---Definir la funcion cuatroIguales tal que (cuatroIguales x y z u) se verifica
---si los elementos x, y, z y u son iguales ,Por ejemplo.
+--Definir la funcion cuatroIguales tal que (cuatroIguales x y z u) se verifica si los elementos x, y, z y u son iguales ,Por ejemplo.
 ------cuatroIguales  5 5 5 5 == True
 ------cuatroIguales  5 5 4 5 == False
 --indicacion utlizar la funcion anterior
