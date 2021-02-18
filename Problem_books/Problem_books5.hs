921d77d59381d3af7ac38600cdfd8b84bce86762
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-18 19:22:52 +0100

    .

---
+++ Problem_books/Problem_books5.hs
@@ -0,0 +1,6 @@
+--La función succ toma cualquier cosa que tenga definido un sucesor y devuelve ese sucesor. Como puedes ver, simplemente hemos separado el nombre de la función
+y su parámetro por un espacio. Llamar a una función con varios parámetros es igual de sencillo. Las funciones min y max toman dos cosas que puedan ponerse en 
+orden (¡cómo los números!) y devuelven uno de ellos.--
+
+ghci> succ 8
+9
