cbf98161f03e5e8979b34816b10fd2ccb3c15cd6
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-18 19:49:17 +0100

    .

---
+++ Problem_books/Problem_books6.hs
@@ -0,0 +1,9 @@
+--La aplicación de funciones (llamar a una función poniendo un espacio después de ella y luego escribir sus parámetros) tiene la máxima prioridad. Dicho 
+--con un ejemplo, estas dos sentencias son equivalentes:
+
+ghci> min 9 10
+9
+ghci> min 3.4 3.2
+3.2
+ghci> max 100 101
+101
