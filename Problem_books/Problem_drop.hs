bf8eab0ece87b3f87c4903c9f28122309871221c
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-19 21:58:35 +0100

    .

---
+++ Problem_books/Problem_drop.hs
@@ -0,0 +1,8 @@
+--drop funciona de forma similar, solo que quita un número de elementos del comienzo de la lista.
+
+ghci> drop 3 [8,4,2,1,5,6]
+[1,5,6]
+ghci> drop 0 [1,2,3,4]
+[1,2,3,4]
+ghci> drop 100 [1,2,3,4]
+[]
