18d4009c145d6d4a9d6a8336bec85b77ab1d62f4
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-19 21:59:52 +0100

    .

---
+++ Problem_books/Problem_take.hs
@@ -0,0 +1,10 @@
+--take toma un número y una lista y extrae dicho número de elementos de una lista. Observa.
+
+ghci> take 3 [5,4,3,2,1]
+[5,4,3]
+ghci> take 1 [3,9,3]
+[3]
+ghci> take 5 [1,2]
+[1,2]
+ghci> take 0 [6,6,6]
+[]
