7c16210dfd406bb5ff1b3c96d5285fedece12385
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-19 21:57:33 +0100

    .

---
+++ Problem_books/Problem_max&min.hs
@@ -0,0 +1,7 @@
+--maximum toma una lista de cosas que se pueden poner en algún tipo de orden y devuelve el elemento más grande.
+--minimum devuelve el más pequeño.
+
+ghci> minimum [8,4,2,1,5,6]
+1
+ghci> maximum [1,9,2,3,4]
+9
