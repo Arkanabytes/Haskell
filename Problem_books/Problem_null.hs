555961582972897a7fd0f44ab047a4897e8de9ad
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-19 22:02:17 +0100

    .

---
+++ Problem_books/Problem_null.hs
@@ -0,0 +1,7 @@
+--null comprueba si una lista está vacía. Si lo está, devuelve True, en caso contrario devuelve False. Usa esta función en lugar de xs == [] (si tienes una lista 
+--que se llame xs).
+
+ghci> null [1,2,3]
+False
+ghci> null []
+True
