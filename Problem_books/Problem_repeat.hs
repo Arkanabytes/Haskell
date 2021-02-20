1b30bc1c2ba009cc838cc3bf3f8d8d34359f27b9
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-20 23:36:27 +0100

    .

---
+++ Problem_books/Problem_repeat.hs
@@ -0,0 +1,4 @@
+--repeat toma un elemento y produce una lista infinita que contiene ese único elemento repetido. Es como hacer un ciclo de una lista con un solo elemento.
+
+ghci> take 10 (repeat 5)
+[5,5,5,5,5,5,5,5,5,5]
