4067bdd17a716fba4b320edfa95c50d07072dae0
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-21 21:02:06 +0100

    .

--- Problem_books/Problem_snd.hs
+++ Problem_books/Problem_snd.hs
@@ -1,4 +1,6 @@
 --snd toma una dupla y devuelve su segundo componente. ¡Sorpresa!
+--Nota Estas funciones solo operan sobre duplas. No funcionaran sobre triplas, cuádruplas, quíntuplas, etc. Veremos más formas de extraer datos de las tuplas un poco más
+--tarde.
 
 ghci> snd (8,11)
 11
