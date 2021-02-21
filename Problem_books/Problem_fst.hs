dd733d2124bc747a3b46b8c3bf323cc9b3edd2ec
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-21 21:03:21 +0100

    .

--- Problem_books/Problem_fst.hs
+++ Problem_books/Problem_fst.hs
@@ -4,3 +4,7 @@ ghci> fst (8,11)
 8
 ghci> fst ("Wow", False)
 "Wow"
+
+
+--Nota Estas funciones solo operan sobre duplas. No funcionaran sobre triplas, cuádruplas, quíntuplas, etc. Veremos más formas de extraer datos de las tuplas un 
+--poco más tarde.
