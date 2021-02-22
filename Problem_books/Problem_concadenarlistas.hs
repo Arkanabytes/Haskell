ec1614c47a7a653f677092b3bdb8dd1d4fa6e2d5
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-22 18:12:04 +0100

    .

---
+++ Problem_books/Problem_concadenarlistas.hs
@@ -0,0 +1,10 @@
+--Una tarea común es concatenar dos listas. Cosa que conseguimos con el operador ++.
+
+ghci> [1,2,3,4] ++ [9,10,11,12]
+[1,2,3,4,9,10,11,12]
+
+ghci> "hello" ++ " " ++ "world"
+"hello world"
+
+ghci> ['w','o'] ++ ['o','t']
+"woot"
