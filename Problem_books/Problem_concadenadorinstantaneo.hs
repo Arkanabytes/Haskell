a20dd220b4017c082089fdd5f855f46b71f6a913
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-22 18:16:29 +0100

    .

---
+++ Problem_books/Problem_concadenadorinstantaneo.hs
@@ -0,0 +1,6 @@
+--Sin embargo, concatenar algo al principio de una lista utilizando el operador : (también llamado operador cons) es instantáneo.
+
+ghci> 'U':"n gato negro"
+"Un gato negro"
+ghci> 5:[1,2,3,4,5]
+[5,1,2,3,4,5]
