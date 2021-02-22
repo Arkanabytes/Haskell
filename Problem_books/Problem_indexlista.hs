efa43fe6738a29c6dfd2a5b1d62ec15faa1ce880
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-22 18:21:31 +0100

    .

---
+++ Problem_books/Problem_indexlista.hs
@@ -0,0 +1,8 @@
+--Si queremos obtener un elemento de la lista sabiendo su índice, utilizamos !!. Los índices empiezan por 0.
+
+--Ejemplos con letras
+ghci> "Steve Buscemi" !! 6
+'B'
+--Ejemplos con numeros
+ghci> [9.4,33.2,96.2,11.2,23.25] !! 1
+33.2
