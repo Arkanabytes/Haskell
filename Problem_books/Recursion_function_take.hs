4dce989ec15ec749f2204a55b2383f8218030e9d
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-23 22:10:58 +0100

    .

--- Problem_books/Recursion_function_take.hs
+++ Problem_books/Recursion_function_take.hs
@@ -3,3 +3,8 @@
 --obtener 0 o menos elementos de una lista, obtendremos una lista vacía. También si intentamos tomar algo de una lista vacía, obtendremos una lista vacía. Fíjate 
 --que ambos son casos base. Vamos a escribirlo.
 
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
