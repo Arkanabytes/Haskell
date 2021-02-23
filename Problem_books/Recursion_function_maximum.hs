3e9c79edd53d12d15b8401d8177d120307444e9c
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-23 21:59:32 +0100

    .

---
+++ Problem_books/Recursion_function_maximum.hs
@@ -0,0 +1,4 @@
+--Recursion
+--La función maximum toma una lista de cosas que pueden ser ordenadas (es decir instancias de la clase de tipos Ord) y devuelve la más grande. Piensa en como 
+--implementaríamos esto de forma imperativa. Probablemente crearíamos una variable para mantener el valor máximo hasta el momento y luego recorreríamos los 
+--elementos de la lista de forma que si un elemento es mayor que el valor máximo actual, lo remplazaríamos. El máximo valor que se mantenga al final es el resultado.
