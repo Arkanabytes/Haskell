977d447b7d7f9a1f50fe69d124ebe485af4c470a
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-24 23:50:54 +0100

    .

---
+++ Problem_books/Problem_quicksort.hs
@@ -0,0 +1,8 @@
+¿Recuerdas nuestra función quicksort del capítulo anterior? Usamos listas por comprensión para filtrar los elementos que eran menores o iguales
+y mayores que el pivote. Podemos conseguir lo mismo de forma más legible usando filter.
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter (<=x) xs)
+        biggerSorted = quicksort (filter (>x) xs)
+    in  smallerSorted ++ [x] ++ biggerSorted
