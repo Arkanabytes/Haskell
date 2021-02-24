72a089e112aaa4f9bf2baf31ea2b734975ef0a90
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-24 23:51:36 +0100

    .

--- Problem_books/Problem_quicksort.hs
+++ Problem_books/Problem_quicksort.hs
@@ -1,4 +1,4 @@
---¿Recuerdas nuestra función quicksort del capítulo anterior? Usamos listas por comprensión para filtrar los elementos que eran menores o iguales
+-- ¿Recuerdas nuestra función quicksort del capítulo anterior? Usamos listas por comprensión para filtrar los elementos que eran menores o iguales
 --y mayores que el pivote. Podemos conseguir lo mismo de forma más legible usando filter.
 
 quicksort :: (Ord a) => [a] -> [a]
