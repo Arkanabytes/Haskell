622748f1d39d2b0d9d7842adcded4e4d295ed78c
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-25 22:00:08 +0100

    .

---
+++ Problem_books/Modulos.hs
@@ -0,0 +1,7 @@
+
+
+--La sintaxis para importar módulos en un script de Haskell es import <module name>. Debe aparecer antes de que definamos cualquier función, así que las 
+--importaciones de módulos suelen estar al principio de los ficheros. Un script puede, obviamente, importar varios módulos. Simplemente hay que poner cada import 
+--en líneas separadas. Vamos a importar el módulo Data.List, el cual contiene un puñado de útiles funciones para trabajar con listas, y utilizaremos una función 
+--que exporta dicho módulo para crear una función que nos diga cuantos elementos únicos hay en una lista.
+
