f6ee5d7ffd226e96025b34b209b753adf1ac3374
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-26 23:49:14 +0100

    .

--- Problem_books/Recursion_function_replicate.hs
+++ Problem_books/Recursion_function_replicate.hs
@@ -1,4 +1,5 @@
 --Recursion
+
 -- Ahora que sabemos cómo pensar de forma recursiva en general, vamos a implementar unas cuantas funciones de forma recursiva. En primer lugar, vamos a implementar 
 --replicate. replicate toma un Int y algún elemento y devuelve una lista que contiene varias repeticiones de ese mismo elemento. Por ejemplo, replicate 3 5 devuelve
 --[5,5,5]. Vamos a pensar en el caso base. Mi intuición me dice que el caso base es 0 o menos. Si intentamos replicar algo 0 o menos veces, debemos devolver una 
