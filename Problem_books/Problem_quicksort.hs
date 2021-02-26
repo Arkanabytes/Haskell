24004423f659a554dee4fe8cdf6be5903e408950
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-26 23:52:52 +0100

    .

--- Problem_books/Problem_quicksort.hs
+++ Problem_books/Problem_quicksort.hs
@@ -1,3 +1,5 @@
+--Funcion quicksort
+
 -- ¿Recuerdas nuestra función quicksort del capítulo anterior? Usamos listas por comprensión para filtrar los elementos que eran menores o 
 -- iguales y mayores que el pivote. Podemos conseguir lo mismo de forma más legible usando filter.
 
