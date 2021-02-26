133032371521caa568b7b11a48f916f395a9d5f3
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-26 23:48:59 +0100

    .

--- Problem_books/Recursion_function_take.hs
+++ Problem_books/Recursion_function_take.hs
@@ -1,4 +1,5 @@
 --Recursion
+
 --Ahora vamos a implementar take. Esta función toma un cierto número de elementos de una lista. Por ejemplo, take 3 [5,4,3,2,1] devolverá [5,4,3]. Si intentamos 
 --obtener 0 o menos elementos de una lista, obtendremos una lista vacía. También si intentamos tomar algo de una lista vacía, obtendremos una lista vacía. Fíjate 
 --que ambos son casos base. Vamos a escribirlo.
