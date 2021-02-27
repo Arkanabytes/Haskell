5c082e7ab5228eb72203490cdef4cdf77599f161
Alejandra Pinto <copinto-@student.42.us.org>
2021-02-27 23:23:27 +0100

    .

--- Problem_books/Recursion_function_repeat.hs
+++ Problem_books/Recursion_function_repeat.hs
@@ -1,4 +1,5 @@
 --Recursion
+
 --Como Haskell soporta listas infinitas, en realidad nuestra recursión no tiene porque tener casos base. Pero si no los tiene, seguiremos calculando algo 
 --infinitamente o bien produciendo una estructura infinita. Sin embargo, lo bueno de estas listas infinitas es que podemos cortarlas por donde queramos. repeat toma
 --un elemento y devuelve una lista infinita que simplemente tiene ese elemento. Una implementación recursiva extremadamente simple es:
